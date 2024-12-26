6177630f7dacd4d731750a995671d4694854b874
Kazarmshchikov.A <Kazarmshchikov.A@cdek.ru>
2024-12-26 19:39:41 +0700

    HW3: refactoring

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -2,7 +2,6 @@ package hw03frequencyanalysis
 
 import (
 	"regexp"
-	"slices"
 	"sort"
 	"strings"
 )
@@ -39,39 +38,21 @@ func Top10(str string) []string {
 	strTrm := reSpace.ReplaceAllString(str, " ")
 	strSpt := strings.Split(strTrm, " ")
 
-	words := make(map[string]int, len(strSpt))
-	sortedWords := make([]word, 0, len(strSpt))
-	result := make([]string, 0, len(strSpt))
+	words := make(map[string]int)
+	sortedWords := make([]word, 0)
+	result := make([]string, 0)
 
 	for _, v := range strSpt {
 		v = cleanStr(v)
 		if v == "" {
 			continue
 		}
-
-		_, ok := words[v]
-		if ok {
-			words[v]++
-		} else {
-			words[v] = 1
-		}
+		words[v]++
 	}
 
 	for k, v := range words {
 		sortedWords = append(sortedWords, word{str: k, hit: v})
 	}
-
-	slices.SortFunc(sortedWords, func(a, b word) int {
-		if a.hit > b.hit {
-			return -1
-		}
-
-		if a.hit == b.hit {
-			return 0
-		}
-
-		return 1
-	})
 
 	sort.Slice(sortedWords, func(i, j int) bool {
 		if sortedWords[i].hit == sortedWords[j].hit {
