790b51b4fc2dbb0b5816fb70b61e457cf2a04388
Kazarmshchikov.A <Kazarmshchikov.A@cdek.ru>
2025-01-02 20:12:49 +0700

    HW4: Fix lint

--- hw04_lru_cache/cache_test.go
+++ hw04_lru_cache/cache_test.go
@@ -1,12 +1,13 @@
 package hw04lrucache
 
 import (
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 	"math/rand"
 	"strconv"
 	"sync"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestCache(t *testing.T) {
