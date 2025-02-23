a6b2668dd975bf9fa985b008a104c2e892afbc91
Kazarmshchikov.A <Kazarmshchikov.A@cdek.ru>
2025-02-24 03:09:06 +0700

    HW10 is completed

--- hw10_program_optimization/stats_test.go
+++ hw10_program_optimization/stats_test.go
@@ -1,3 +1,4 @@
+//go:build !bench
 // +build !bench
 
 package hw10programoptimization
@@ -31,9 +32,46 @@ func TestGetDomainStat(t *testing.T) {
 		require.Equal(t, DomainStat{"browsedrive.gov": 1}, result)
 	})
 
+	t.Run("find 'net'", func(t *testing.T) {
+		result, err := GetDomainStat(bytes.NewBufferString(data), "net")
+		require.NoError(t, err)
+		require.Equal(t, DomainStat{"teklist.net": 1}, result)
+	})
+
 	t.Run("find 'unknown'", func(t *testing.T) {
 		result, err := GetDomainStat(bytes.NewBufferString(data), "unknown")
 		require.NoError(t, err)
 		require.Equal(t, DomainStat{}, result)
 	})
+
+	t.Run("invalid 'org'", func(t *testing.T) {
+		str := `{"Id":1,"Name":"John Doe","Username":"JonDoe","Email":"invalidEmail.org","Phone":"146-91-01","Password":"acSBF5","Address":"Russell Trail 61"}`
+		result, err := GetDomainStat(bytes.NewBufferString(str), "org")
+		require.NoError(t, err)
+		require.Equal(t, DomainStat{}, result)
+	})
+
+	t.Run("invalid json", func(t *testing.T) {
+		str := `invalid`
+		result, err := GetDomainStat(bytes.NewBufferString(str), "gov")
+		require.Error(t, err)
+		require.Nil(t, result)
+	})
+}
+
+// go test -count=5 -bench=BenchmarkGetDomainStat -benchmem -benchtime 10s > new.txt
+func BenchmarkGetDomainStat(b *testing.B) {
+	b.ReportAllocs()
+	data := `{"Id":1,"Name":"Howard Mendoza","Username":"0Oliver","Email":"aliquid_qui_ea@Browsedrive.gov","Phone":"6-866-899-36-79","Password":"InAQJvsq","Address":"Blackbird Place 25"}
+{"Id":2,"Name":"Jesse Vasquez","Username":"qRichardson","Email":"mLynch@broWsecat.com","Phone":"9-373-949-64-00","Password":"SiZLeNSGn","Address":"Fulton Hill 80"}
+{"Id":3,"Name":"Clarence Olson","Username":"RachelAdams","Email":"RoseSmith@Browsecat.com","Phone":"988-48-97","Password":"71kuz3gA5w","Address":"Monterey Park 39"}
+{"Id":4,"Name":"Gregory Reid","Username":"tButler","Email":"5Moore@Teklist.net","Phone":"520-04-16","Password":"r639qLNu","Address":"Sunfield Park 20"}
+{"Id":5,"Name":"Janice Rose","Username":"KeithHart","Email":"nulla@Linktype.com","Phone":"146-91-01","Password":"acSBF5","Address":"Russell Trail 61"}`
+
+	for i := 0; i < b.N; i++ {
+		_, err := GetDomainStat(bytes.NewBufferString(data), "example.com")
+		if err != nil {
+			b.Fatalf("unexpected error: %v", err)
+		}
+	}
 }
